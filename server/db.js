b94d57c02953d83632e1c246a9b221de749b7a25
guoxu09 <guoxu09@58.com>
2021-07-09 16:51:49 +0800

    mongodb配置文件修改

--- server/db.js
+++ server/db.js
@@ -1,15 +1,25 @@
 // 设置 Mongoose 连接
-const mongoose = require('mongoose'); // 引入mongoose
-//连接到bigdata数据库
-const mongoDB = 'mongodb://localhost:27017/wechatroom';
+const mongoose = require('mongoose') // 引入mongoose
+// 连接到bigdata数据库
+const username = 'root'
+const pwd = 'password'
+const host = 'localhost'
+const post = '27017'
+const database = 'wechatroom'
+const mongoDB = `mongodb://${ username }:${ pwd }@${ host }:${ post }/${ database }`
+// const mongoDB = `mongodb://${ host }:${ post }/${ database }`
 // 让 mongoose 使用全局 Promise 库
-mongoose.Promise = global.Promise;
-mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true}, function (err) {
-  if (err) {
-    console.log('Connection Error:' + err);
-  } else {
-    console.log('Connection Success!');
+mongoose.Promise = global.Promise
+mongoose.connect(
+  mongoDB,
+  { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false },
+  function (err) {
+    if (err) {
+      console.log('Connection Error:' + err)
+    } else {
+      console.log('Connection Success!')
+    }
   }
-});
+)
 
-module.exports = mongoose;
+module.exports = mongoose
